22b74f10f7d3fc6563cc75f0b6c31907a0338e98
Evgen <padkin@techart.ru>
2025-08-20 15:15:39 +0300

    sosalsa

---
+++ main.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const apiUrl = 'https://randomuser.me/api/';
+    const userGender = document.querySelector('.profile__userGender');
+    const userPicture = document.querySelector('.profile__userPhoto');
+    const userPhoneNumber = document.querySelector('.profile__userPhoneNumber');
+    const userName = document.querySelector('.profile__userName');
+
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then((json) => {
+        console.log(json.results[0])
+        
+        const userGenderValue = json.results[0].gender
+        const userPictureValue = json.results[0].picture.large
+        const userPhoneNumberValue = json.results[0].phone
+        const userNameValue = json.results[0].name.title
+
+        userGender.innerHTML = `
+            <div>Пол: ${userGenderValue}</div>
+        `
+
+        userPicture.innerHTML = `
+            <img src="${userPictureValue}">
+        `
+
+        userPhoneNumber.innerHTML = `
+            <div>Номер телефона: ${userPhoneNumberValue}</div>
+        `
+
+        userName.innerHTML = `
+            <div>${userNameValue}</div>
+        `
+      })
+sfafasf
+})
